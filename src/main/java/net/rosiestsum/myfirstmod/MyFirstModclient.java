44c992595094a3ef01784a55f49345bf7f1cc406
Frankie Davis <frankie@Regans-MacBook-Air.local>
2025-07-26 17:51:04 -0500

    Initial commit

---
+++ src/main/java/net/rosiestsum/myfirstmod/MyFirstModclient.java
@@ -0,0 +1,10 @@
+package net.rosiestsum.myfirstmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MyFirstModclient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
