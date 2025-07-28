6d9278f2ad6a88cc9e700dff13b02b8e0c5ce880
frankie <goombapop45@gmail.com>
2025-07-27 19:06:16 -0500

    added pink garnet and raw pink garnet not smeltable yet

--- src/main/java/net/rosiestsum/myfirstmod/MyFirstMod.java
+++ src/main/java/net/rosiestsum/myfirstmod/MyFirstMod.java
@@ -2,6 +2,7 @@ package net.rosiestsum.myfirstmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.rosiestsum.myfirstmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,6 +16,7 @@ public class MyFirstMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
